43586fc8c497bfdc393d4639732a23a4b18c6b9b
sait han uzun <saithan.uzun@gmail.com>
2023-03-12 21:45:53 +0000

    Add Stack

--- Linked-list/Program.cs
+++ Linked-list/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Node<T>
+{
+  public Node<T> Next;
+  public T Data;
+}
+
+class LinkedList<T>
+{
+    private Node<T> head;
+    
+    public void GetAllNodes()
+    {
+        Node<T> iterator = head;
+        while (iterator is not null)
+        {
+            Console.WriteLine(iterator.Data);
+            iterator = iterator.Next;
+        }
+    }
+
+    public void Add(T data)
+    {
+        Node<T> myNode = new() { Data = data, Next = null };
+        if (head is null)
+            head = myNode;
+        else
+        {
+            Node<T> iterator = head;
+            while (iterator.Next is not null)
+            {
+                iterator = iterator.Next;
+            }
+            iterator.Next = myNode;
+        }
+    }
+    
+}
+
+class Program
+{ 
+    public static void Main(string[] args)
+    {
+        LinkedList<string> myList = new();
+        myList.Add("Hello");
+        myList.Add("World");
+      
+
+        myList.GetAllNodes();
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,140 @@
+﻿class Node<T>
+{ 
+   public Node<T>? Next;
+   public T? Data;
+}
+
+interface IStack<T>
+{
+    public int Count { get; } 
+    void Push(T data);
+    T Pop();
+    T Peek();
+    void Clear();
+}
+class Stack<T> : IStack<T>
+{
+    private Node<T> _head;
+    private Node<T> Head { get => _head; set => _head=value; }
+    public int Count
+    {
+        get
+        {
+            int _count = 0;
+            Node<T> iterator = new();
+            iterator = Head;
+            while (iterator is not null)
+            {
+                _count ++;
+                iterator = iterator.Next;
+            }
+            return _count;
+        }
+    }
+
+    public void Push(T data)
+    {
+        Node<T> myNode = new() { Next = null, Data = data };
+        if (Head is null)
+            Head = myNode;
+        else
+        {
+            myNode.Next = Head;
+            Head = myNode;
+        }
+    }
+
+    public T Pop()
+    {
+        T data = Head.Data;
+        Head = Head.Next;
+        return data;
+    }
+
+    public T Peek()
+    {
+        return Head.Data;
+    }
+
+    public void Clear()
+    {
+        Node<T> newHead = new();
+        Head = newHead;
+    }
+}
+
+class Stack2<T> : IStack<T>
+{
+    private int _size;
+    private Node<T> _head;
+    private Node<T> Head { get => _head; set => _head = value; }
+    private Node<T> _current;
+    private Node<T> Current
+    {
+        get
+        {
+            Current = Head;
+            for (int i = 1; i<_size ; i++)
+            {
+                Current = _current.Next;
+            } 
+            return _current;
+        }
+        set => _current = value;
+    }
+    
+    public int Count => _size;
+    public void Push(T data)
+    {
+        Node<T> myNode = new() {Next = null , Data = data };
+        
+        if (Head is null)
+        {
+            Head = myNode;
+            Current = Head;
+        }
+        else
+            Current.Next = myNode;
+        
+        _size += 1;
+    }
+    
+    public T Pop()
+    {
+        _size -= 1;
+        return Current.Data;
+    }
+    
+    public T Peek()
+    {
+        return Current.Data;
+    }
+
+    public void Clear()
+    {
+        Head = null;
+        _size = 0;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //new Stack() or Stack2()
+        IStack<string> myStack = new Stack<string>();
+        
+        myStack.Push("hello");
+        myStack.Push("world");
+        
+        Console.WriteLine(myStack.Count);
+        Console.WriteLine(myStack.Peek());
+        myStack.Pop();
+        Console.WriteLine(myStack.Peek());
+        myStack.Clear();
+        Console.WriteLine("clear");
+        myStack.Push("hello");
+        myStack.Push("again");
+        Console.WriteLine(myStack.Peek());
+    }
+}
